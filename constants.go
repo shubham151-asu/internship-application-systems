c197d554c2dc731926c4e25a9763f9688c591212
Shubham <spraka21@asu.edu>
2020-04-19 21:07:34 -0700

    constant to be used by all code

---
+++ constants.go
@@ -0,0 +1,18 @@
+package main
+
+
+const (
+	ProtocolIvP4 = 1
+	ProtocolIvP6 = 58
+	ReadDeadline = 3
+	ReadDataSize = 64
+	DefaultCount = 64
+	DefaultTTL = 59
+	DefaultDelay = 1
+	ListenAddrICMP = "0.0.0.0"	
+	ListenAddrIPv4 = "0.0.0.0"
+	ListenAddrIPv6 = "::" 
+	messageString = "Hey! how are you doing"
+	maxMessageLength = 30
+)
+
